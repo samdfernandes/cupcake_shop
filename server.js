b8f881fc43d9f7c40c50a0958e422e124d4ff85e
Sammy <asfernandes@sbcglobal.net>
2019-08-02 11:21:52 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+//___________________________________
+//
+// Dependencies
+//___________________________________
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+//___________________________________
+//
+// Port
+//___________________________________
+const PORT = process.env.PORT || 3000
+
+//___________________________________
+//
+// Database
+//___________________________________
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/cupcakesAndOtherSweets'
+
+
+//mongo connection
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, () => {
+    console.log('CONNECTED TO MONGO')
+})
+
+//___________________________________
+//
+// Routes
+//___________________________________
+//localhost: 3000
+app.get('/', (req, res) => {
+    res.send('app is running')
+})
+
+
+//___________________________________
+//
+// Listen
+//___________________________________
+app.listen(PORT, () => console.log('Listening on Port: ', PORT));
